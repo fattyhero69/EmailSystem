bb8eea4ce93e1b016c13a2d21f071f85b1239d42
Tamzid Choudhury <74117270+fattyhero69@users.noreply.github.com>
2022-04-28 05:41:34 -0400

    Added creation of multiple users

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EmailSystem
- A mini email creation tool
+ A mini email creation tool. Application asks for users information and company name in console.
+The application provides an email address associated with the user's provided information. A random 
+password is also generated for the user, and is allowed to be changed once if the user wants a 
+different password of the same length or a new password of different length.
--- src/emailapp/emailapp.java
+++ src/emailapp/emailapp.java
@@ -1,10 +1,20 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class emailapp {
     public static void main(String[] args) {
-        EmailObjs em1 = new EmailObjs();
-//        System.out.println(em1.getGeneratedPassword());
-//        System.out.println(em1.getMailboxCapacity());
-        System.out.println(em1.getUserInfo());
+        // Single instance
+//        EmailObjs em1 = new EmailObjs();
+//        System.out.println(em1.getUserInfo());
+        System.out.println("How many people are being added to the system?");
+        Scanner in = new Scanner(System.in);
+        int userNum = in.nextInt();
+        EmailObjs[] emails = new EmailObjs[userNum];
+        // Create i email address
+        for (int i = 0; i < userNum; i++) {
+            emails[i] = new EmailObjs();
+            System.out.println(emails[i].getUserInfo());
+        }
     }
 }
